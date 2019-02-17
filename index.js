1a6a9fddfa5225dd3c8417c8e1b947d884dcbd1a
Kehinde Orilogbon <archkenny@yahoo.com>
2019-02-17 23:07:42 +0100

    Initial and saved commit

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+
+/**
+ * Only logs to the console in devolopment mode
+ *
+ */
+
+const devLog = (...args) => {
+  if (process.env.NODE_ENV === "development") {
+    args.forEach((arg, i) => {
+      console.log(`arg ${i} => `, arg);
+    });
+    return args;
+  }
+};
+
+module.exports = devLog;
---
+++ test/test.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var expect = require("chai").expect;
+var devLogger = require("../index");
+
+// describe("#devLogger", function() {
+//   it("log all arguments", function() {
+//     var result = devLogger(1);
+//     expect(result).to.equal(1);
+//   });
+// });
